cb3a3f08d233069df9e151989a1a17094c1415d4
swflynn <swflynn@uci.edu>
2019-07-25 15:48:51 -0700

    2D Uniform Grid

--- scripts/2D_stdnorm/unif_grid/unif_grid.f90
+++ scripts/2D_stdnorm/unif_grid/unif_grid.f90
@@ -1,8 +1,8 @@
 !=============================================================================80
-!                       Metropolis Monte Carlo Code Grid
+!                             Uniform Grid
 !==============================================================================!
 !       Discussion:
-!Generate 2D uniform grid for testing purposes
+!Generate 2D uniform grid for numerical integraion.
 !==============================================================================!
 !       Modified:
 !   30 April 2019
@@ -12,52 +12,50 @@
 program main
 implicit none
 !==============================================================================!
-integer::NG_1D,Nparticles,i,j,counter
-double precision::lower,upper,alpha_par
-double precision,allocatable,dimension(:)::points,alpha
-double precision,allocatable,dimension(:,:)::x
-lower=-10
-upper=25
-NG_1D=20
-Nparticles=NG_1D**2
-alpha_par=1d0
+!               Discussion:
 !==============================================================================!
-allocate(points(NG_1D),x(2,Nparticles),alpha(Nparticles))
-do i=1,NG_1D
-    points(i)=lower+(i-1.)*(upper-lower)/(NG_1D-1.)
+!NP_1D          ==>Number of points across a single dimension 
+!Npoints        ==>Total Number of Points
+!lower          ==>Lower Bound
+!upper          ==>Upper Bound
+!points         ==>Single Dimension Coordinates
+!x              ==>Grid Coordinates
+!==============================================================================!
+integer::NP_1D,Npoints,i,j,counter
+double precision::lower,upper
+double precision,allocatable,dimension(:)::points
+double precision,allocatable,dimension(:,:)::x
+read(*,*) lower
+read(*,*) upper
+read(*,*) NP_1D
+Npoints=NP_1D**2
+!==============================================================================!
+allocate(points(NP_1D),x(2,Npoints))
+!==============================================================================!
+!                           1D Uniform Distribution
+!==============================================================================!
+do i=1,NP_1D
+    points(i)=lower+(i-1.)*(upper-lower)/(NP_1D-1.)
 enddo
+!==============================================================================!
+!                            2D Combinatiorics
+!==============================================================================!
 counter=1
-do i=1,NG_1D
-    do j=1,NG_1D
+do i=1,NP_1D
+    do j=1,NP_1D
         x(1,counter)=points(i)
         x(2,counter)=points(j)
         counter=counter+1
     enddo
 enddo
-write(*,*) 'Test 3; Successfully Generated Gaussian Centers'
-!==============================================================================!
-!                       Generate Alpha Scaling 
-!Input Paramater (same for every dimension)
-!==============================================================================!
-alpha=alpha_par
 !==============================================================================!
 !                   Write Gridpoints Configuration to file 
 !==============================================================================!
 open(unit=17,file='grid.dat')
-do i=1,Nparticles
+do i=1,Npoints
     write(17,*) x(:,i)
 enddo
 close(17)
-write(*,*) 'Test 2; Successfully Generated Quasi-Regular Gridpoints'
-!==============================================================================!
-!                          Generate Alpha Scaling 
-!==============================================================================!
-open(unit=18,file='alphas.dat')
-do i=1,Nparticles
-    write(18,*) alpha(i)
-enddo
-close(18)
-write(*,*) 'Test 3; Successfully Generated Gaussian Widths'
 !==============================================================================!
 write(*,*) 'Hello Universe!'
 end program main
