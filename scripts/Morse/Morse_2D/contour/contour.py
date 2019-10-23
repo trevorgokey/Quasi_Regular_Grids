6fa1ab1ae2d5244977bfaf87ade268fa045b76af
swflynn <swflynn@uci.edu>
2019-10-22 19:47:52 -0700

    Update Contor Plots

--- scripts/Morse/Morse_2D/contour/contour.py
+++ scripts/Morse/Morse_2D/contour/contour.py
@@ -1,7 +1,7 @@
 #=============================================================================80
-#                Contour Plot for Henon-Heiles Potential
+#                Contour Plot for Morse Potential
 #=============================================================================80
-#Python Contour Plot of the 2D-Henon-Heiles Potential
+#Python Contour Plot of the 2D-Morse Potential
 #super-impose grid onto the contour plot.
 #Currently makes a 4-panel figure, requires 4 external data sets (x y1 y2...y_n)
 #==============================================================================#
@@ -19,9 +19,11 @@ import matplotlib.pyplot as plt
 #2D Henon-Heiles Potential
 #lambda         ==>Scaling Parameter (see literature for value)
 #==============================================================================#
-def HH_pot(x,y):
-    lambd=np.sqrt(0.0125)
-    return 0.5*(x**2+y**2)+lambd*(x**2*y-y**3/3.)
+def morse_pot(x,y):
+    omega_x=0.2041241
+    omega_y=0.18371169
+    D_morse=12.0
+    return D_morse*((np.exp(-omega_x*x)-1)**2+(np.exp(-omega_y*y)-1)**2)
 #==============================================================================#
 #                             Program Main
 #==============================================================================#
@@ -37,86 +39,76 @@ def HH_pot(x,y):
 #==============================================================================#
 #                    Read in External Grid Points to Data Frame
 #==============================================================================#
-script,grid_50,grid_100,grid_200,grid_300=argv
-df_50=pd.read_csv(grid_50,delim_whitespace=True,header=None,dtype=np.float64)
-df_100=pd.read_csv(grid_100,delim_whitespace=True,header=None,dtype=np.float64)
-df_200=pd.read_csv(grid_200,delim_whitespace=True,header=None,dtype=np.float64)
-df_300=pd.read_csv(grid_300,delim_whitespace=True,header=None,dtype=np.float64)
+script,grid_1,grid_2,grid_3,grid_4=argv
+df_1=pd.read_csv(grid_1,delim_whitespace=True,header=None,dtype=np.float64)
+df_2=pd.read_csv(grid_2,delim_whitespace=True,header=None,dtype=np.float64)
+df_3=pd.read_csv(grid_3,delim_whitespace=True,header=None,dtype=np.float64)
+df_4=pd.read_csv(grid_4,delim_whitespace=True,header=None,dtype=np.float64)
 #==============================================================================#
 #                           Define Parameters
 #==============================================================================#
-Ncols=len(df_50.columns)
+Ncols=len(df_1.columns)
 Nint=100
-xmin=-8
-xmax=8
-ymin=-6
-ymax=8.5
+xmin=-5
+xmax=20
+ymin=-5
+ymax=22
 #==============================================================================#
 #                             Global Contour
 #==============================================================================#
 x=np.linspace(xmin,xmax,Nint)
 y=np.linspace(ymin,ymax,Nint)
 X,Y=np.meshgrid(x,y)
-Z=HH_pot(X,Y)
+Z=morse_pot(X,Y)
 #==============================================================================#
 #                         Inner Contours (Hard-Coded)
 #==============================================================================#
-x1=np.linspace(-7,8,Nint)
-y1=np.linspace(-7,8,Nint)
+#x1=np.linspace(-7,8,Nint)
+#y1=np.linspace(-7,8,Nint)
+x1=np.linspace(xmin,xmax,Nint)
+y1=np.linspace(ymin,ymax,Nint)
 X1,Y1=np.meshgrid(x1,y1)
-Z1=HH_pot(X1,Y1)
-level=[0.5,5,10,12.9]
-#==============================================================================#
-#                              Ecut(Hard-Coded)
-#==============================================================================#
-level2=[12.9]
+Z1=morse_pot(X1,Y1)
+level=[12]
 #==============================================================================#
 #                             Plot Grid + Contours
 #==============================================================================#
-x_50=df_50[0]
-x_100=df_100[0]
-x_200=df_200[0]
-x_300=df_300[0]
+x_1=df_1[0]
+x_2=df_2[0]
+x_3=df_3[0]
+x_4=df_4[0]
 #==============================================================================#
 fig,axs=plt.subplots(2,2)
-#==============================================================================#
-my_labels = np.arange(-6,8,2)
+my_labels = np.arange(-5,23,5)
 my_dict = {'fontsize': 30}
+#==============================================================================#
 for i in range(1,Ncols):
-    axs[0,0].scatter(x_50,df_50[i],c="black",s=14)
-axs[0,0].contour(X,Y,Z,colors='royalblue')
-axs[0,0].contour(X1,Y1,Z1,colors='royalblue',levels=level)
-axs[0,0].contour(X1,Y1,Z1,colors='red',levels=level2)
+    axs[0,0].scatter(x_1,df_1[i],c="black",s=14)
+axs[0,0].contour(X1,Y1,Z1,colors='red',levels=level)
 axs[0,0].set_xticks([])
 axs[0,0].set_yticks(my_labels)
 axs[0,0].set_yticklabels(labels = my_labels, fontdict = my_dict)
 #==============================================================================#
 for i in range(1,Ncols):
-    axs[0,1].scatter(x_100,df_100[i],c="black",s=14)
-axs[0,1].contour(X,Y,Z,colors='royalblue')
-axs[0,1].contour(X1,Y1,Z1,colors='royalblue',levels=level)
-axs[0,1].contour(X1,Y1,Z1,colors='red',levels=level2)
+    axs[0,1].scatter(x_2,df_2[i],c="black",s=14)
+axs[0,1].contour(X1,Y1,Z1,colors='red',levels=level)
 axs[0,1].set_xticks([])
 axs[0,1].set_yticks([])
 #==============================================================================#
 for i in range(1,Ncols):
-    axs[1,0].scatter(x_200,df_200[i],c="black",s=14)
-axs[1,0].contour(X,Y,Z,colors='royalblue')
-axs[1,0].contour(X1,Y1,Z1,colors='royalblue',levels=level)
-axs[1,0].contour(X1,Y1,Z1,colors='red',levels=level2)
+    axs[1,0].scatter(x_3,df_3[i],c="black",s=14)
+axs[1,0].contour(X1,Y1,Z1,colors='red',levels=level)
 axs[1,0].set_xticklabels(labels = my_labels, fontdict = my_dict)
 axs[1,0].set_yticklabels(labels = my_labels, fontdict = my_dict)
 axs[1,0].set_xticks(my_labels)
 axs[1,0].set_yticks(my_labels)
 #==============================================================================#
 for i in range(1,Ncols):
-    axs[1,1].scatter(x_300,df_300[i],c="black",s=14)
-axs[1,1].contour(X,Y,Z,colors='royalblue')
-axs[1,1].contour(X1,Y1,Z1,colors='royalblue',levels=level)
-axs[1,1].contour(X1,Y1,Z1,colors='red',levels=level2)
+    axs[1,1].scatter(x_4,df_4[i],c="black",s=14)
+axs[1,1].contour(X1,Y1,Z1,colors='red',levels=level)
 axs[1,1].set_xticklabels(labels = my_labels, fontdict = my_dict)
 axs[1,1].set_xticks(my_labels)
 axs[1,1].set_yticks([])
 #==============================================================================#
 plt.show()
-fig.savefig("plot.pdf", bbox_inches = 'tight')
+fig.savefig('plot.pdf', bbox_inches='tight')
