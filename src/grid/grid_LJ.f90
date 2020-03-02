83597e90de4a750c867a3458d6d285151eb55b05
Trevor Gokey <trevorgokey@gmail.com>
2020-03-01 17:46:30 -0800

    clean up code and file options

--- src/grid/grid_LJ.f90
+++ src/grid/grid_LJ.f90
@@ -138,66 +138,67 @@ contains
 
         allocate( U_move(this%points_nr), s(this%system%d), x0(this%system%d))
 
+        open(unit=20,file='grid.xyz', action='WRITE')
+        close(20)
+
         do i=1,N_MC
-        newmove = 0
-        write(*,*) i,N_MC
-        do k=1, Npoints
-        !==============================================================================!
-        !                       Randomly Select Atom to Move
-        !==============================================================================!
-            call random_number(s) 
-            !k=random_integer(1,Npoints, s(1))
-        !==============================================================================!
-        !                           Generate trial move
-        !        random numbers generated (0,1), make it (-1,1) ==> s=2*s-1
-        !==============================================================================!
-            x0=this%X(:,k)+mv_cutoff*(2*s-1)
-        !==============================================================================!
-        !                   Only consider point if V(trial) < Ecut 
-        !                   Compute Energy Change due to Trial Move
-        !==============================================================================!
-            !print*, "x0 is", x0
-            V = this%system%V( x0)
-            if( V < this%system%E_cut) then
-                counter=counter+1
-                Delta_E=0d0
-                P = this%system%P(x0)
-                U_move(k)= P
-                !print*, "Umove prior to loop is ", U_move(k), "allocated?", allocated(U_move), " Valloc? ", allocated(this%Vij)
-        !  !$OMP parallel default(none) shared(Npoints,this%Vij,this%X,k,U_move,this%system%X,Natoms,x0) private(j,diff) reduction(+:Delta_E)
-        !  $OMP p#arallel default(none) shared(Npoints,Vij,X,k,U_move,x0) private(j,diff) reduction(+:Delta_E)
-        !$OMP parallel default(none) shared(Npoints,this,k,U_move,x0) private(j,diff) reduction(+:Delta_E)
-        !$OMP do 
-                do j=1,Npoints
-                    !write(*,*) "k in parallel is", k, "j is", j
-                    if(j.ne.k) then
-                        U_move(j)=this%Pair_LJ_NRG( this%X(:,j), x0)
-                        Delta_E = Delta_E+ ( this%Vij(j,k) - U_move(j))
-                        !write(*,*) j,k,"Ujk=", this%Vij(j,k), " DE= ", Delta_E, " trial U_move was", U_move(j), &
-                        !    & "leading to ", this%Vij(j,k)-U_move(j)
+            newmove = 0
+            write(*,*) i,N_MC
+            do k=1, Npoints
+            !==============================================================================!
+            !                       Randomly Select Atom to Move
+            !==============================================================================!
+                call random_number(s) 
+                !k=random_integer(1,Npoints, s(1))
+            !==============================================================================!
+            !                           Generate trial move
+            !        random numbers generated (0,1), make it (-1,1) ==> s=2*s-1
+            !==============================================================================!
+                x0=this%X(:,k)+mv_cutoff*(2*s-1)
+            !==============================================================================!
+            !                   Only consider point if V(trial) < Ecut 
+            !                   Compute Energy Change due to Trial Move
+            !==============================================================================!
+                !print*, "x0 is", x0
+                V = this%system%V( x0)
+                if( V < this%system%E_cut) then
+                    counter=counter+1
+                    Delta_E=0d0
+                    P = this%system%P(x0)
+                    U_move(k)= P
+                    !print*, "Umove prior to loop is ", U_move(k), "allocated?", allocated(U_move), " Valloc? ", allocated(this%Vij)
+            !  !$OMP parallel default(none) shared(Npoints,this%Vij,this%X,k,U_move,this%system%X,Natoms,x0) private(j,diff) reduction(+:Delta_E)
+            !  $OMP p#arallel default(none) shared(Npoints,Vij,X,k,U_move,x0) private(j,diff) reduction(+:Delta_E)
+            !$OMP parallel default(none) shared(Npoints,this,k,U_move,x0) private(j,diff) reduction(+:Delta_E)
+            !$OMP do 
+                    do j=1,Npoints
+                        !write(*,*) "k in parallel is", k, "j is", j
+                        if(j.ne.k) then
+                            U_move(j)=this%Pair_LJ_NRG( this%X(:,j), x0)
+                            Delta_E = Delta_E+ ( this%Vij(j,k) - U_move(j))
+                            !write(*,*) j,k,"Ujk=", this%Vij(j,k), " DE= ", Delta_E, " trial U_move was", U_move(j), &
+                            !    & "leading to ", this%Vij(j,k)-U_move(j)
+                        endif
+                    enddo
+            !$OMP end do
+            !$OMP end parallel
+                    !write(*,*) "Delta_E from reduce is", Delta_E
+            !==============================================================================!
+            !                  Accept any trial that decreases the energy 
+            !==============================================================================!
+                    !write(*,*) "new is", U_move(k), "old was", this%Vij(k,k), "P(x) is", P
+                    if(Delta_E>0d0)then
+                        this%Vij(:,k)=U_move(:)
+                        this%Vij(k,:)=U_move(:)
+                        accept=accept+1
+                        newmove = 1
+                        this%X(:,k)=x0(:)
+                        deltae1=deltae1+Delta_E
+                        !write(*,*) k, Delta_E, "Accepted! Displacement was", &
+                        !    & sqrt(sum((mv_cutoff*(2*s-1))**2)), "V(x0)", V
                     endif
-                enddo
-        !$OMP end do
-        !$OMP end parallel
-                write(*,*) "Delta_E from reduce is", Delta_E
-        !==============================================================================!
-        !                  Accept any trial that decreases the energy 
-        !==============================================================================!
-                write(*,*) "new is", U_move(k), "old was", this%Vij(k,k), "P(x) is", P
-                if(Delta_E>0d0)then
-                    this%Vij(:,k)=U_move(:)
-                    this%Vij(k,:)=U_move(:)
-                    accept=accept+1
-                    newmove = 1
-                    this%X(:,k)=x0(:)
-                    deltae1=deltae1+Delta_E
-                    write(*,*) k, Delta_E, "Accepted! Displacement was", &
-                        & sqrt(sum((mv_cutoff*(2*s-1))**2)), "V(x0)", V
-
-
                 endif
-            endif
-        enddo
+            enddo
         if( newmove > 0) then
             open(unit=20,file='grid.xyz', access='APPEND')
         
@@ -222,17 +223,18 @@ contains
             if (mv_cutoff < .01) then
                 mv_cutoff = .01
             endif
-            accept=0
-            counter=0
             !call Moments(Moment,x)
             !write(*,*) 'Moments:'
             !write(*,*) Moment(1:9)
-            write(*,*) 'mv cutoff', mv_cutoff
-            write(*,*) 'Deltae1==>', deltae1
+            write(*,*) 'mv cutoff=', mv_cutoff
+            write(*,*) 'Deltae1=  ', deltae1
+            write(*,*) 'accepted= ', accept
+            accept=0
+            counter=0
             plt_count=plt_count+1
-            write(18,*) plt_count, mv_cutoff
-            write(19,*) plt_count, deltae1
-            deltae1=0
+            !write(18,*) plt_count, mv_cutoff
+            !write(19,*) plt_count, deltae1
+            deltae1=0.0
         endif
         !write(*,*) "wrote xyz"
         !open(unit=21,file='grid.dat')
@@ -241,8 +243,8 @@ contains
         !    enddo
         !close(21)
     enddo
-    close(18)
-    close(19)
+    !close(18)
+    !close(19)
 
     end subroutine
 
