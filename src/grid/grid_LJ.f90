b9b2db04658a88b403a00b8cd654f6d6b8dc8fb4
Trevor Gokey <trevorgokey@gmail.com>
2020-03-03 01:33:09 -0800

    clean up output; add tunables; add T to MC accept

--- src/grid/grid_LJ.f90
+++ src/grid/grid_LJ.f90
@@ -114,18 +114,20 @@ contains
 
         double precision, dimension(:), allocatable :: s, x0
         double precision, dimension(:), allocatable :: U_move
-        double precision :: Delta_E, mv_cutoff=1.0, deltae1=0.0, V, P
-        integer :: i, j, k, counter=0, Npoints, accept=0, plt_count=0, MMC_freq, N_MC, newmove=0
+        double precision :: Delta_E, mv_cutoff=1.0, deltae1=0.0, V, V_trial, P, E, SDX
+        integer :: i, j, k, counter=0, Npoints, accept=0, hotaccept=0,plt_count=0, MMC_freq, N_MC, newmove=0
         !double precision, dimension(this%points_nr,this%points_nr)  :: Vij
         !double precision, dimension(3,this%points_nr)  :: X
-
+        !character(len=*), parameter :: fmt = "(I8,A1,I10,A5,F6.3,A5,E8.6,A5,E8.6,A5,E8.6,5A,E8.6,A7,F6.3,5A,I4,I6)"
+        character(len=*), parameter :: fmt = '(I10, " /", I10, "   cLJ=", ES13.6, "   DX=", ES13.6, "   GE=", ES13.6, &   
+            &"   GDE=", ES13.6,"   SE=", ES13.6, "   <SDX>= ", ES13.6, "   acc=", F8.4, "  hot=", I4)'
 
         !Vij = this%Vij
         !X   = this%X
         
         obj = this
 
-        print*, "GRID_LJ OPTIMIZER", this%opt_tune, this%cLJ
+        print*, "GRID_LJ OPTIMIZER", this%opt_tune, this%cLJ, " steps=", N_MC
 
         MMC_freq = this%opt_tune
         N_MC     = this%opt_steps
@@ -138,30 +140,40 @@ contains
 
         allocate( U_move(this%points_nr), s(this%system%d), x0(this%system%d))
 
-        open(unit=20,file='grid.xyz', action='WRITE')
+        open(unit=20,file='grid.xyz', status='REPLACE')
         close(20)
 
-        do i=1,N_MC
+        do i=1,Npoints
+            do j=1, i-1
+                this%Vij(j,i) = this%Pair_LJ_NRG( this%X(:,j), this%X(:,i))
+            enddo
+            do j=1, i-1
+                this%Vij(i,j) = this%Vij(j,i)
+            enddo
+            this%Vij(i,i) = 0.0
+        enddo
+        i = 0
+        do
+            i = i + 1
+            if ( N_MC == 0 .or. ( N_MC > -1 .and. i == N_MC)) then
+                exit
+            endif
             newmove = 0
-            write(*,*) i,N_MC
+            !write(*,*) i,N_MC
+            E = sum(this%Vij) / 2
+            V = 0.0
+            SDX = 0.0
             do k=1, Npoints
-            !==============================================================================!
-            !                       Randomly Select Atom to Move
-            !==============================================================================!
+                V = V + this%system%V( this%X(:,k))
+            enddo
+            do k=1, Npoints
+
                 call random_number(s) 
-                !k=random_integer(1,Npoints, s(1))
-            !==============================================================================!
-            !                           Generate trial move
-            !        random numbers generated (0,1), make it (-1,1) ==> s=2*s-1
-            !==============================================================================!
-                x0=this%X(:,k)+mv_cutoff*(2*s-1)
-            !==============================================================================!
-            !                   Only consider point if V(trial) < Ecut 
-            !                   Compute Energy Change due to Trial Move
-            !==============================================================================!
+                x0=this%X(:,k)+mv_cutoff*(2.0*s-1.0)
+                !print*, "DELTA ", mv_cutoff*(2.0*s-1.0)
                 !print*, "x0 is", x0
-                V = this%system%V( x0)
-                if( V < this%system%E_cut) then
+                V_trial = this%system%V( x0)
+                if( V_trial < this%system%E_cut) then
                     counter=counter+1
                     Delta_E=0d0
                     P = this%system%P(x0)
@@ -183,57 +195,71 @@ contains
             !$OMP end do
             !$OMP end parallel
                     !write(*,*) "Delta_E from reduce is", Delta_E
-            !==============================================================================!
-            !                  Accept any trial that decreases the energy 
-            !==============================================================================!
                     !write(*,*) "new is", U_move(k), "old was", this%Vij(k,k), "P(x) is", P
-                    if(Delta_E>0d0)then
+                    if(Delta_E>0d0 .or. exp(Delta_E) > .5) then
                         this%Vij(:,k)=U_move(:)
                         this%Vij(k,:)=U_move(:)
                         accept=accept+1
                         newmove = 1
-                        this%X(:,k)=x0(:)
-                        deltae1=deltae1+Delta_E
                         !write(*,*) k, Delta_E, "Accepted! Displacement was", &
-                        !    & sqrt(sum((mv_cutoff*(2*s-1))**2)), "V(x0)", V
+                        !    & sqrt(sum((mv_cutoff*(2*s-1))**2)), "V(x0)", V, this%X(:,k), x0! " DX ", sqrt(sum((this%X(:,k)-x0(:))**2))
+                        SDX = SDX + sqrt(sum((this%X(:,k)-x0(:))**2))
+                        deltae1=deltae1+Delta_E
+                        E = E - deltae1
+                        V = V + (V_trial - this%system%V( this%X(:,k) ))
+                        if (exp(Delta_E) > .5) then
+                            hotaccept=hotaccept+1
+                        endif
+                        this%X(:,k)=x0(:)
                     endif
                 endif
             enddo
         if( newmove > 0) then
             open(unit=20,file='grid.xyz', access='APPEND')
-        
                 write(20,*) Npoints
                 write(20,*)
                 do j=1,Npoints
                     write(20,*) "C     ", this%X(:,j)
                 enddo
             close(20)
+            newmove = 0
         end if
-    !==============================================================================!
-    !                           Update Cutoff Paramater
-    !           acceptance rate ~50%, adjust trial displacement accordingly
-    !==============================================================================!
+        
         if(mod(i,MMC_freq)==0)then
-            write(*,*) 'MMC Iteration', i
+        !character(len=*), parameter :: fmt = '(I10, "/", I10, " cLJ ", E6.3, " DX=", E6.3, " GE=", E7.4, " GDE=", E7.4," SE ",'&
+        !    &'E7.4, " <SDX> ", E6.3, " acc=", I7, "/", I8)'
+            print fmt, i, N_MC, this%cLJ, mv_cutoff, E, deltae1, V, SDX, dble(accept)/dble(Npoints * MMC_freq), hotaccept
             if(dble(accept)/counter<0.5)then 
-                mv_cutoff=mv_cutoff*0.9
+                mv_cutoff=mv_cutoff*0.999
             else
-                mv_cutoff=mv_cutoff*1.1
+                mv_cutoff=mv_cutoff*1.001
             endif
-            if (mv_cutoff < .01) then
-                mv_cutoff = .01
-            endif
+            mv_cutoff = max( mv_cutoff, 1e-2)
+            !if (mv_cutoff < .0001) then
+            !    mv_cutoff = .0001
+            !endif
             !call Moments(Moment,x)
             !write(*,*) 'Moments:'
             !write(*,*) Moment(1:9)
-            write(*,*) 'mv cutoff=', mv_cutoff
-            write(*,*) 'Deltae1=  ', deltae1
-            write(*,*) 'accepted= ', accept
-            accept=0
-            counter=0
-            plt_count=plt_count+1
             !write(18,*) plt_count, mv_cutoff
             !write(19,*) plt_count, deltae1
+            if ( abs(deltae1) < this%opt_eps .and. dble(accept)/dble(Npoints*MMC_freq) > .2 ) then
+                print*, "**CONVERGED ENERGY**"
+                return
+            end if
+            !if ( mv_cutoff < 1e-5) then
+            !    print*, "**CONVERGED TO STATIONARY POINT**"
+            !    return
+            !end if
+            if ( deltae1 == 0.0 ) then
+                this%cLJ = this%cLJ * .999
+            else
+                this%cLJ = this%cLJ * 1.001
+            endif
+            accept=0
+            hotaccept=0
+            counter=0
+            plt_count=plt_count+1
             deltae1=0.0
         endif
         !write(*,*) "wrote xyz"
